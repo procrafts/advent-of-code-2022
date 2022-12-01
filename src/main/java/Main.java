934174b18e918614288bc75f4d39fe4eecfa3368
Franz Fabian Schmid <ff.schmid@gmail.com>
2022-12-01 07:59:02 +0100

    implement challenge solution 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,53 @@
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
 
+    }
+
+    public static int run1(String fileName) throws URISyntaxException, IOException {
+        File file = readFile(fileName);
+        List<String> example = convertFileToStrings(file);
+        LinkedList<LinkedList<String>> emptyList = new LinkedList<>();
+        emptyList.add(new LinkedList<>());
+        LinkedList<LinkedList<String>> splitInGroups = example.stream().reduce(emptyList, Main::group, (l, i) -> l);
+        LinkedList<Integer> summedUpGroups = splitInGroups.stream().map(l -> l.stream().mapToInt(Integer::parseInt).sum()).collect(Collectors.toCollection(LinkedList::new));
+        Optional<Integer> a = summedUpGroups.stream().max(Integer::compare);
+        return a.get();
+    }
+
+    public static LinkedList<LinkedList<String>> group(LinkedList<LinkedList<String>> list, String s) {
+        if (s.equals("")) {
+            list.add(new LinkedList<>());
+        } else {
+            list.getLast().add(s);
+        }
+        return list;
+    }
+
+
+    public static File readFile(String fileName) throws URISyntaxException {
+        ClassLoader classLoader = Main.class.getClassLoader();
+        URL resource = classLoader.getResource(fileName);
+        if (resource == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        } else {
+            return new File(resource.toURI());
+        }
+    }
+
+    public static List<String> convertFileToStrings(File file) throws IOException {
+        List<String> lines;
+        lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
+        return lines;
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,5 +1,45 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MainTest {
+    @Test
+    void runMain_shouldCompile() {
+        Main.main(new String[0]);
+        assertTrue(true);
+    }
 
+    @Test
+    void Main_convertFileToStrings() throws URISyntaxException, IOException {
+        assertEquals(Arrays.stream("""
+                1000
+                2000
+                3000
+                                
+                4000
+                                
+                5000
+                6000
+                                
+                7000
+                8000
+                9000
+                                
+                10000""".split("\n")).toList(), Main.convertFileToStrings(Main.readFile("example1.txt")));
+    }
+
+    @Test
+    void Main_run1() throws URISyntaxException, IOException {
+        int exampleResult = Main.run1("example1.txt");
+        assertEquals(24000, exampleResult);
+        int result = Main.run1("input1.txt");
+        System.out.println("-----");
+        System.out.println(result);
+        System.out.println("-----");
+    }
 }
