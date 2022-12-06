a6f6f86ac7d69f7c6b8855ae0feb690a316ffafa
Franz Fabian Schmid <ff.schmid@gmail.com>
2022-12-06 07:37:01 +0100

    add challenge 6 solutions (node.js)

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ require('./src/main/resources/nodejs/challenge02');
 require('./src/main/resources/nodejs/challenge03');
 require('./src/main/resources/nodejs/challenge04');
 require('./src/main/resources/nodejs/challenge05');
+require('./src/main/resources/nodejs/challenge06');
--- src/main/resources/nodejs/challenge06.js
+++ src/main/resources/nodejs/challenge06.js
@@ -1,5 +1,34 @@
 const readInput = require('./utils/readInput');
+const sum = require('./utils/sum');
 
-const overview = readInput(6).split("\n");
+const MarkerType = {Package: 4, Message: 14};
 
-console.log([0, 0]);
+const signals = readInput(6).split("\n");
+const sum1 = signals.map(getFirstMarkerFn(MarkerType.Package)).reduce(sum, 0);
+const sum2 = signals.map(getFirstMarkerFn(MarkerType.Message)).reduce(sum, 0);
+console.log([sum1, sum2]);
+
+function getFirstMarkerFn(markerLength) {
+    return (signal) => {
+        let pointer = 0;
+        const group = [];
+        let marker = 0;
+        while (marker === 0 && pointer < signal.length) {
+            const target = signal.charAt(pointer);
+            group.push(target);
+            if (group.length > markerLength) {
+                group.shift();
+                if (isUnambiguity(group, markerLength)) {
+                    marker = pointer + 1;
+                }
+            }
+            pointer++;
+        }
+        return marker;
+    }
+}
+
+function isUnambiguity(group, markerLength) {
+    const result = new Set(group);
+    return result.size === markerLength;
+}
